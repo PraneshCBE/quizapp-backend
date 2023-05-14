ed9007864cf74c9bd042a6972be97de6d4c4445c
pr <rvpspran@gmail.com>
2023-05-14 22:38:15 +0530

    changes

--- src/main/java/com/bmd/learnspringboot/service/JwtUtil.java
+++ src/main/java/com/bmd/learnspringboot/service/JwtUtil.java
@@ -1,27 +1,32 @@
 package com.bmd.learnspringboot.service;
 
 import com.bmd.learnspringboot.model.RequestBody.LoginRequestBody;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.*;
 
+import static io.jsonwebtoken.Jwts.builder;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 @Component
 
 
 public class JwtUtil {
+
+    public HashMap<String,String> userTokenList = new HashMap<>();
 
     @Value("${jwt.secret}")
     private String SECRET_KEY;
 
     private String createToken(Map<String, Object> claims, List<String> subject) {
         long now = System.currentTimeMillis();
-        Date validity = new Date(now); // 1 hour
-        return Jwts.builder().setClaims(claims).setSubject(subject.toString()).setIssuedAt(new Date(now))
-                .setExpiration(validity).signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+        var userToken = builder().setClaims(claims).setSubject(subject.toString()).setIssuedAt(new Date(now))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+        userTokenList.put(subject.get(0),userToken);
+        return userToken;
     }
     public String generateToken(LoginRequestBody login) {
         Map<String, Object> claims = new HashMap<>();
@@ -32,6 +37,8 @@ public class JwtUtil {
 
         return createToken(claims, a);
     }
+
+
 
 
 }
